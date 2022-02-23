ecda44d024a9426a3dadf6d4e95de47f8901a776
Zhigang Gao <gzg1984@aliyun.com>
2022-02-23 16:21:28 +0800

    Last save of troob init project. Try To Move to New UBUNTU plat

--- docker_auto.sh
+++ docker_auto.sh
@@ -1,18 +1,27 @@
 #!/bin/sh
-. projectFile.sh
+export JRE_HOME=/opt/jdk1.8.0_161/
+
+
+. ./projectFile.sh
 
 #update lxr_db.tb_project_file set file_path='/test/curl.sh' where uuid='testuuid' ;
-TARGETROOT=/opt
+
+
+#TARGETROOT=/opt
+TARGETROOT=/opt/files
 #TARGETROOT=/opt/file_root
 TARGETPROJECTROOT=${TARGETROOT}/project_base
 TARGETINDEXROOT=${TARGETROOT}/index_base
 SHELLPWD=$PWD
+
+
 PROJECT=$1
 if [ "$PROJECT" = "" ]
 then
     echo "Need Project Full Path"
     exit 255
 fi
+
 
 if [ ! -d "$PROJECT" ]
 then
@@ -22,14 +31,12 @@ fi
 
 
 
-
-
-MYSQLENTRY=`./getmysql.sh`
 #echo $PROJECT
 projectName=$(basename $PROJECT)
 #echo $projectBase
 projectFolder=$(dirname $PROJECT)
 #echo $projectFolder
+
 
 echo "Copying Source Folder..."
 cd $projectFolder
@@ -39,48 +46,40 @@ cp -rf $projectName $TARGETPROJECTROOT/
 
 # cd /Library/tomcat/bin
 # sudo ./shutdown.sh
-$MYSQLENTRY  <<!
+#mysqlsh --sql -h 127.0.0.1 -u root  <<!
+mysql -h 127.0.0.1 -u root -proot lxr_db  <<!
 delete from lxr_db.tb_project_base where title='$projectName';
-insert into lxr_db.tb_project_base (title,\`desc\`,source_from,download_count,visit_count,is_show,add_time,index_path,zip_url,title_en,status)
+insert into lxr_db.tb_project_base (title,\`desc\`,source_from,download_count,visit_count,is_show,add_time,index_path,zip_url,title_en,status) 
 values ('$projectName','$projectName','$projectName',0,0,1,now(),'$projectName','$projectName','$projectName',0);
 !
 
-$MYSQLENTRY <<! > temp.log
+
+mysql -h 127.0.0.1 -u root -proot lxr_db <<! > temp.log
 select project_id from  lxr_db.tb_project_base where title='$projectName';
 !
 
+
 PROJECTID=`grep -v project_id temp.log`
-if [ "$PROJECTID" = "" ]
-then
-    echo "Cannot get Project ID, quit"
-    exit 253
-fi
+
 echo "Now $projectName has ID $PROJECTID"
 
 OLDPWD=`pwd`
 cd $projectFolder/$projectName
 
 
-
 recEchoMysqlCommandForFolder $PROJECT  >/tmp/tempInsert.log
 
+mysql -h 127.0.0.1 -u root -proot lxr_db < /tmp/tempInsert.log
 
-$MYSQLENTRY < /tmp/tempInsert.log
-#echo $OLDPWD
 
 echo "Creating Index Folder..."
 rm -rf $projectFolder/Index
 cd $SHELLPWD
-java -jar target/index-0.0.3-SNAPSHOT.jar -r $TARGETPROJECTROOT -p $projectName
-#java -jar $SHELLPWD/target/index-0.0.3-SNAPSHOT.jar  -p $projectName
-#/opt/jdk1.8.0_161/bin/java -jar target/index-0.0.3-SNAPSHOT.jar -r /opt/files/project_base -p TroobInit
-
+#java -jar target/index-0.0.3-SNAPSHOT.jar -r $TARGETPROJECTROOT -p $projectName
+/opt/jdk1.8.0_161/bin/java -jar target/index-0.0.3-SNAPSHOT.jar -r /opt/files/project_base -p TroobInit
 
 
 echo "Copying Index Folder..."
 cd $projectFolder
 rm -rf $TARGETINDEXROOT/$projectName
 cp -rf $TARGETPROJECTROOT/Index $TARGETINDEXROOT/$projectName
-
-# cd /Library/tomcat/bin
-# sudo ./startup.sh
